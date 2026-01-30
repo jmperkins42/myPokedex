a6d9e28d93d00161daa8d48bbc30946d121e75f9
Jonah Perkins <jonah.m.perkins@gmail.com>
2026-01-29 19:33:41 -0600

    attempting to use pokeapi to get data

--- src/app.js
+++ src/app.js
@@ -2,6 +2,19 @@ import $ from 'jquery';
 import 'datatables.net-bs5';
 import 'datatables.net-bs5/css/dataTables.bootstrap5.css';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
   $('#tblPokemon').DataTable();
+  
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/1`);
+  const p = await res.json();
+
+  table.row.add([
+    pokemon.id.toString().padStart(3, '0'),
+    pokemon.name,
+    "grass",
+    "abilities",
+    "baseStats",
+    "Kanto"
+  ]).draw();
+  
 });
