47abafe346d1e5505e6f24ac63fc9ce66e1945ad
Jonah Perkins <jonah.m.perkins@gmail.com>
2026-01-30 16:16:54 -0600

    successfully added a row to the data table

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@ import 'datatables.net-bs5';
 import 'datatables.net-bs5/css/dataTables.bootstrap5.css';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  $('#tblPokemon').DataTable();
+  const table = $('#tblPokemon').DataTable();
   
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/1`);
   const p = await res.json();
 
   table.row.add([
-    pokemon.id.toString().padStart(3, '0'),
-    pokemon.name,
+    p.id.toString().padStart(3, '0'),
+    p.name,
     "grass",
     "abilities",
     "baseStats",
