13bbe83a5332213d241ebae12c92ab56f896c929
Jonah Perkins <jonah.m.perkins@gmail.com>
2026-01-31 11:45:18 -0600

    fixed formatting by adding stat specific columns

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ import 'datatables.net-bs5/css/dataTables.bootstrap5.css';
 // things that need to happen on page load
 document.addEventListener('DOMContentLoaded', async () => {
   // creates dataTable
-  const table = $('#tblPokemon').DataTable();
+  const table = $('#tblPokemon').DataTable({
+    paging: true,
+    searching: true,
+    ordering: true
+  });
   
   // fetch calls to pokeapi grabbing the first pokemon
   let url = 'https://pokeapi.co/api/v2/pokemon?limit=100';
@@ -23,17 +27,23 @@ document.addEventListener('DOMContentLoaded', async () => {
   const pokemonRef = allPokemon[0];
   const res = await fetch(pokemonRef.url);
   const pokemon = await res.json();
-  // using map to get specific data about the pokemon
-  const types = pokemon.types.map(t => t.type.name).join(', ');
-  const abilities = pokemon.abilities.map(a => a.ability.name).join(', ');
-  const stats = pokemon.stats.map(s => `${s.stat.name}: ${s.base_stat}`).join(', ');
+  // using map to get base stat data
+  const statMap = Object.fromEntries(
+    pokemon.stats.map(s => [s.stat.name, s.base_stat])
+  );
   // since the dataTable already exists, we can add a row this way rather than adding to the HTML
   table.row.add([
     pokemon.id.toString().padStart(3, '0'),
     pokemon.name,
-    types,
-    abilities,
-    stats
+    pokemon.types.map(t => t.type.name).join(', '),
+    pokemon.abilities.map(a => a.ability.name).join(', '),
+
+    statMap.hp,
+    statMap.attack,
+    statMap.defense,
+    statMap['special-attack'],
+    statMap['special-defense'],
+    statMap.speed
   ]).draw();
   
 });
